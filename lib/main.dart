b8986711f2349454a4eb93223fcdfe39eb4e037e
iamarjun <arjunmanoj1995@gmail.com>
2020-06-07 11:53:18 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: WorldTime(),
+  ));
+}
+
+class WorldTime extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
