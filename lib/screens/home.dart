53ee14c9295236ccdd535ebf6b4f653ae5d84653
iamarjun <arjunmanoj1995@gmail.com>
2020-06-07 15:30:16 +0530

    show useful data on the home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,33 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:worldtime/service/WorldTime.dart';
 
 class Home extends StatefulWidget {
-
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-
   Map data;
 
   @override
   Widget build(BuildContext context) {
-
     data = ModalRoute.of(context).settings.arguments;
+
+    WorldTime worldTime = data['world time'];
+    var time = DateFormat.jm().format(DateTime.parse(worldTime.datetime));
+    var location = worldTime.timezone;
 
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            FlatButton.icon(
-              onPressed: () => Navigator.pushNamed(context, '/location'),
-              icon: Icon(
-                Icons.edit_location,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 120.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              FlatButton.icon(
+                onPressed: () => Navigator.pushNamed(context, '/location'),
+                icon: Icon(
+                  Icons.edit_location,
+                ),
+                label: Text('Edit Location'),
               ),
-              label: Text(data['world time'].datetime),
-            )
-          ],
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    location,
+                    textScaleFactor: 2,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                time,
+                textScaleFactor: 3,
+              )
+            ],
+          ),
         ),
       ),
     );
